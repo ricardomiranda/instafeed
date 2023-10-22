0a23d6e16c753ed8728e90258e385ed6f90d0df4
RCM <mail@ricardoMiranda.com>
2023-10-22 03:21:42 +0100

    instagram logo

--- my-project/instagram-feed/InstagramFeed.js
+++ my-project/instagram-feed/InstagramFeed.js
@@ -12,7 +12,7 @@ import { Feather } from '@expo/vector-icons';
 import Stories from './Stories';
 import data from './data';
 
-const INSTAGRAM_LOG =
+const INSTAGRAM_LOGO =
   'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png';
 
 export default function Instagram() {
@@ -34,10 +34,16 @@ export default function Instagram() {
       <StatusBar style="dark" />
       <View style={styles.header}>
         <TouchableOpacity>
-          <Feather name="camera" size={24} color="black" />
+          <Feather name="camera" size={24} />
         </TouchableOpacity>
-        <StatusBar style="auto" />
+
+        <Image source={{ uri: INSTAGRAM_LOGO }} style={styles.logo} />
+
+        <TouchableOpacity>
+          <Feather name="send" size={24} />
+        </TouchableOpacity>
       </View>
+
       <FlatList
         data={data.articles}
         keyExtractor={item => item.id.toString()}
@@ -74,5 +80,20 @@ const styles = StyleSheet.create({
   itemImage: {
     width: '100%',
     height: 200,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  logo: {
+    flex: 1,
+    height: 50,
+    resizeMode: 'contain',
   },
 });
